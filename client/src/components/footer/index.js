7c874b2bb81c66a5079cb2570bb9cae04b66fca3
nova <courtois1337@gmail.com>
2020-12-14 20:13:29 -0800

    clean up footer

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -4,9 +4,9 @@ import styles from './footer.module.scss';
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <a href="https://github.com/novacourtois/acab-colors" target="_blank">GitHub</a>
-      <div className ={styles.text}>
-      // created by <a href="">nova skye </a>
+      <a href="https://github.com/novacourtois/acab-colors" target="_blank"  rel="noreferrer noopenner">github</a>
+      <div className={styles.text}>
+      // created by <span>nova skye</span>
       </div>
     </div>
   );
