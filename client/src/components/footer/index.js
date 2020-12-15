6d27e43a02080011fbc52c3b2a3af354fb656346
nova <courtois1337@gmail.com>
2020-12-14 20:16:35 -0800

    fix footer warning

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -6,7 +6,7 @@ export default function Footer() {
     <div className={styles.footer}>
       <a href="https://github.com/novacourtois/acab-colors" target="_blank"  rel="noreferrer noopenner">github</a>
       <div className={styles.text}>
-      // created by <span>nova skye</span>
+        created by <span>nova skye</span>
       </div>
     </div>
   );
