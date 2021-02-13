3e05aea41373db4813f9f95e97801ba1107d7a0a
nova <courtois1337@gmail.com>
2021-02-13 08:43:20 -0800

    update footer repo link

--- client/src/components/footer/index.js
+++ client/src/components/footer/index.js
@@ -4,7 +4,7 @@ import styles from './footer.module.scss';
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      <a href="https://github.com/novacourtois/acab-colors" target="_blank"  rel="noreferrer noopenner">github</a>
+      <a href="https://github.com/anarchotechqueer/acab-colors" target="_blank"  rel="noreferrer noopenner">github</a>
       <div className={styles.text}>
         created by <a href="https://disregardingstandards.com/" target="_blank" rel="noreferrer noopenner">nova skye</a>
       </div>
